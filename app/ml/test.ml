4d0546dd85829782bbc898bcd8f6c721f95450c9
Danny Willems <contact@danny-willems.be>
2016-06-17 13:10:32 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,12 +1,13 @@
 let on_device_ready _ =
-  let accelerometer = Cordova_accelerometer.t () in
-
   let succ =
     fun a ->
       let doc = Dom_html.document in
       let p = Dom_html.createP doc in
-      p##.innerHTML := Js.string ("x: " ^ (string_of_int a#x) ^ "\ny: " ^
-      (string_of_int a#y) ^ "\nz: " ^ (string_of_int a#z));
+      let x_val = Cordova_accelerometer.acceleration_x a in
+      let y_val = Cordova_accelerometer.acceleration_y a in
+      let z_val = Cordova_accelerometer.acceleration_z a in
+      p##.innerHTML := Js.string ("x: " ^ (string_of_int x_val) ^ "\ny: " ^
+      (string_of_int y_val) ^ "\nz: " ^ (string_of_int z_val));
       Dom.appendChild doc##.body p
   in
 
@@ -14,7 +15,7 @@ let on_device_ready _ =
     fun () -> Jsoo_lib.alert "Error with accelerometer"
   in
 
-  let id = accelerometer#watch_acceleration succ err
+  let id = Cordova_accelerometer.watch_acceleration succ err
   (Cordova_accelerometer.create_options ()) in
   ()
 
